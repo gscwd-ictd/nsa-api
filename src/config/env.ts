ec3b4ae165c419619448b033fcd6d967b1e3e1bb
Eric Sison <ericsison.dev@gmail.com>
2024-09-23 10:40:27 +0800

    changed process.env to Bun.env

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,15 +2,15 @@ import { EnvSchema } from "../types/env-schema";
 import { validateEnv } from "../utils/validate-env";
 
 const env = {
-  dbHost: process.env.DB_HOST!,
-  dbPort: parseInt(process.env.DB_PORT!),
-  dbUser: process.env.DB_USER!,
-  dbPassword: process.env.DB_PASS!,
-  dbName: process.env.DB_NAME!,
-  redisHost: process.env.REDIS_HOST!,
-  redisPort: parseInt(process.env.REDIS_PORT!),
-  redisUser: process.env.REDIS_USER,
-  redisPassword: process.env.REDIS_PASS,
+  dbHost: Bun.env.DB_HOST!,
+  dbPort: parseInt(Bun.env.DB_PORT!),
+  dbUser: Bun.env.DB_USER!,
+  dbPassword: Bun.env.DB_PASS!,
+  dbName: Bun.env.DB_NAME!,
+  redisHost: Bun.env.REDIS_HOST!,
+  redisPort: parseInt(Bun.env.REDIS_PORT!),
+  redisUser: Bun.env.REDIS_USER,
+  redisPassword: Bun.env.REDIS_PASS,
 };
 
 export const appConfig = validateEnv<typeof EnvSchema>(env, EnvSchema);
