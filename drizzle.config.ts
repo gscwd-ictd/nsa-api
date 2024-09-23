c236e47f012ba97e8ea9c869a3641b423c94833a
Eric Sison <ericsison.dev@gmail.com>
2024-09-23 11:06:00 +0800

    fixed schema folder lookup file for drizzle generate

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { appConfig } from "@mapstudio/config/env";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/db/schema/*",
+  schema: "./src/db/schema/*.ts",
   out: "./src/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
