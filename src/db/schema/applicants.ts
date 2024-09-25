f9091fa8718986085b24138772cbebd0956d2280
john-henry-alfeche <alfeche.ictd@gmail.com>
2024-09-25 09:12:02 +0800

    added applicants schema and migrations

--- src/db/schema/applicants.ts
+++ src/db/schema/applicants.ts
@@ -1,5 +1,15 @@
-import { pgTable, uuid } from "drizzle-orm/pg-core";
+import { date, pgEnum, pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const sexEnum = pgEnum("sex", ["male", "female"]);
 
 export const applications = pgTable("applicants", {
-  id: uuid("applicant_id").defaultRandom(),
+  id: uuid("applicant_id").primaryKey().defaultRandom(),
+  name: text("name").notNull(),
+  address: text("address").notNull(),
+  email: varchar("email", { length: 50 }),
+  birthdate: date("birth_date").notNull(),
+  sex: sexEnum("sex").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
+  deletedAt: timestamp("deleted_at").defaultNow(),
 });
